a16ef30aa218dccd3f03f4b21b4a7cb60cbe69d7
Abdulahad911 <a.a.khan6204@gmail.com>
2024-09-29 07:33:55 -0700

    changing color removing container and giving whole body background color

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
+
+var today = new Date();
+var currentYear = today.getFullYear();
+
 function addingAgeCalculater(){
+    
     var inputText = document.getElementById('inputText')
     var inputValue = document.getElementById('inputValue')
+    var month = document.getElementById('month')
+    var year = document.getElementById('year')
+    var day = document.getElementById('day')
     if (!inputText.value.trim()){
-              return alert('Enter your age to calculate.')
+              return alert('Invalid format. Please enter the date as mm-dd-yr.')
              }
-             var li = document.createElement('li')
-             li.textContent = inputText.value;
-             inputValue.appendChild(li); 
-             li.className = 'listItem'
-             inputText.value = ''     
+             if(inputText.value < years)  {
+                year.textContent = `${inputText.value} `
+                
+               }  
+              else{
+                alert("Year cann't be in future")
+              }
+              inputText.value = '' 
+              if(inputText.value < 13)  {
+                month.textContent = inputText.value
+               }  
+            else{
+                return alert('invalid month')
+            }
+             
+             inputText.value = '' 
 }  
 
 
