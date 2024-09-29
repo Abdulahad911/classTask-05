fdb5915284121fc2ec5c6b504bcfc6e8cd1d26c8
Abdulahad911 <a.a.khan6204@gmail.com>
2024-09-28 22:50:19 -0700

    adding list item to ul

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function addingAgeCalculater(){
     var inputText = document.getElementById('inputText')
     var inputValue = document.getElementById('inputValue')
+    if (!inputText.value.trim()){
+              return alert('Enter your age to calculate.')
+             }
+             var li = document.createElement('li')
+             li.textContent = inputText.value;
+             inputValue.appendChild(li); 
+             li.className = 'listItem'
+             inputText.value = ''     
 }  
+
+
+
+
+
+
+
+
