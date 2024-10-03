9b4ba943b0f9617a928ec96310acfa5eb98b7eb3
Abdulahad911 <a.a.khan6204@gmail.com>
2024-10-02 23:13:45 -0700

    Calling every element by Id's and adding it into function

--- script.js
+++ script.js
@@ -1,39 +1,10 @@
 
-var today = new Date();
-var currentYear = today.getFullYear();
+function addingAgeCalculater() {
+    var inputText = document.getElementById('inputText').value.trim();
+    var yearElem = document.getElementById('year');
+    var monthElem = document.getElementById('month');
+    var dayElem = document.getElementById('day');
 
-function addingAgeCalculater(){
-    
-    var inputText = document.getElementById('inputText')
-    var inputValue = document.getElementById('inputValue')
-    var month = document.getElementById('month')
-    var year = document.getElementById('year')
-    var day = document.getElementById('day')
-    if (!inputText.value.trim()){
-              return alert('Invalid format. Please enter the date as mm-dd-yr.')
-             }
-             if(inputText.value < years)  {
-                year.textContent = `${inputText.value} `
-                
-               }  
-              else{
-                alert("Year cann't be in future")
-              }
-              inputText.value = '' 
-              if(inputText.value < 13)  {
-                month.textContent = inputText.value
-               }  
-            else{
-                return alert('invalid month')
-            }
-             
-             inputText.value = '' 
-}  
-
-
-
-
-
-
-
-
+   
+    document.getElementById('inputText').value = '';
+}
